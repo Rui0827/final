2174f658c51bb59a622deeb4f733452d2bc948e0
Rui0827 <2201385@s.asojuku.ac.jp>
2024-01-10 10:32:34 +0900

    db-connectの作成

---
+++ bgm/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517370-final';
+const USER = 'LAA1517370';
+const PASS = 'Pass0827';
+
+$connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=UTF8';
+?>
