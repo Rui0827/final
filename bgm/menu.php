afcfcf0288e789058cb5c9b19f7ea07198c75666
Rui0827 <2201385@s.asojuku.ac.jp>
2024-01-10 10:29:27 +0900

    menuの作成とinsertの作成

---
+++ bgm/menu.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>BGM集</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <h2>BGM管理メニュー</h2>
+    <form action="branch.php" method="post">
+        <button type="submit" name="select">一覧</button>
+        <button type="submit" name="insert">登録</button>
+        <button type="submit" name="update">更新</button>
+        <button type="submit" name="delete">削除</button>
+    </form>
+</body>
+
+</html>
