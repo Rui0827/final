978e110204665316b70b8d5d686b2c907878139f
Rui0827 <2201385@s.asojuku.ac.jp>
2024-01-12 12:30:10 +0900

    insertの仮完成

--- bgm/insert-output.php
+++ bgm/insert-output.php
@@ -13,37 +13,31 @@
     <?php
     $pdo=new PDO($connect,USER,PASS);
     $sql=$pdo->prepare('insert into BGMs(bgm_name,sakusha) values(?,?)');
-    (empty($_POST['name'])){
-        echo '商品名を入力してください';
-
-    }(empty)
-    else if($sql->execute([$_POST['id'],$_POST['name'],$_POST['price']])){
+ 
+    $sql->execute([$_POST['name'],$_POST['sakusha']])
         echo '<font color="red">追加に成功しました。</font>';
 
-    }else{
-        echo '<font color="red">追加に失敗しました。</font>';
-
-    }
+   
     ?>
     <br>
     <hr><br>
     <table>
         <tr>
-            <th>商品番号</th>
-            <th>商品名</th>
+            <th>BGM名</th>
+            <th>楽曲者</th>
             <th>価格</th>
             <?php
-    foreach($pdo->query('select * from product') as $row){
+    foreach($pdo->query('select * from BGMs') as $row){
         echo '<tr>';
-        echo '<td>',$row['id'],'</td>';
-        echo '<td>',$row['name'],'</td>';
-        echo '<td>',$row['price'],'</td>';
+        echo '<td>',$row['bgm_id'],'</td>';
+        echo '<td>',$row['bgm_name'],'</td>';
+        echo '<td>',$row['sakusha'],'</td>';
         echo '</tr>';
         echo "\n";
     }
     ?>
     </table>
-    <form action="ren6-5-input.php" method="post">
+    <form action="insert-input.php" method="post">
         <button type="submit">追加画面へ戻る</button>
     </form>
 
