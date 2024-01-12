5e1ef2a13c0aeeff4a29dac4caddb344f7b3f386
Rui0827 <2201385@s.asojuku.ac.jp>
2024-01-12 12:25:56 +0900

    insertの追加

---
+++ bgm/insert-input.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>練習6-5-input</title>
+</head>
+
+<body>
+    <p>商品を追加します</p>
+    <form action="insert-output.php" method="post">
+        BGM名<input type="text" name="bgm_name"><br>
+        作曲者<input type="text" name="sakusha"><br>
+
+        <button type="submit">追加</button>
+    </form>
+</body>
+
+</html>
