12957692b5e98baf882c2d18e640bfdcfe71a2f4
Rui0827 <2201385@s.asojuku.ac.jp>
2024-01-16 09:54:37 +0900

    いろいろな変更と交信

---
+++ bgm/update-input.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>update-final</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>商品番号</th>
+            <th>商品名</th>
+            <th>商品価格</th>
+        </tr>
+        <?php
+    $pdo=new PDO($connect, USER, PASS);
+	$sql=$pdo->prepare('select * from BGMs where id=?');
+	$sql->execute([$_POST['id']]);
+
+
+	foreach ($sql as $row) {
+        echo '<tr>';
+		echo '<form action="update-output.php" method="post">';
+        
+	
+		echo '<td>';
+		echo '<input type="text" name="bgm_name" value="', $row['bgm_name'], '">';
+		echo '</td> ';
+		echo '<td>';
+		echo ' <input type="text" name="sakusha" value="', $row['sakusha'], '">';
+		echo '</td> ';
+		echo '<td><input type="submit" value="更新"></td>';
+		echo '</form>';
+        echo '</tr>';
+		echo "\n";
+	}
+?>
+    </table>
+    <button onclick="location.href='menu.php'">トップへ戻る</button>
+</body>
+
+</html>
